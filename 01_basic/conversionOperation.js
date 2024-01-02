bbf60ee7cbeb2c070f8c82441369136c89a38426
AKSHANSH GUPTA <91845143+Akshansh1911@users.noreply.github.com>
2024-01-02 15:40:50 +0000

    operations

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,43 @@
+let score ="33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+//here NaN means not a number
+
+// conversion operation helps datatypes to change their value in different datatypes 
+//like number to string 
+//string to number 
+// boolean to number or number to boolean and many more like that 
+
+//*****************************OPERATIONS **************/
+
+let value = 3;
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2/3);
+// console.log(2%3);
+// console.log(3%2);
+// console.log(4%2);
+
+
+let str1 = "hello";
+let str2 = " Akshansh"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log(+true);
+
+let Counter =100;
+Counter++;
+console.log(Counter); 
