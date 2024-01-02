7476c9a60ce90d3e3781943da6b32c3be3fda924
AKSHANSH GUPTA <91845143+Akshansh1911@users.noreply.github.com>
2024-01-02 14:46:55 +0000

    add datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,16 @@
+//number
+//string => ""
+//bigInt
+//null
+//undefined
+//boolean =>true/false
+//symbol=>unique
+
+
+
+//object
+
+//typeof used for define the datatype of value using console.log 
+// typeof null is object but for undefined it is undefined
+
+
