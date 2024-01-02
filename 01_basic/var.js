f6f2ccfff2fd5f0093af173ac3458d208da94f42
AKSHANSH GUPTA <91845143+Akshansh1911@users.noreply.github.com>
2024-01-02 14:26:10 +0000

    save new changes

---
+++ 01_basic/var.js
@@ -0,0 +1,21 @@
+const accountId = 12345;
+var accountName = "new";
+let accountdetails = "hello";
+let hellouser;
+
+//always use let for variable because var has some block and functional scope issue 
+
+
+/*
+and if u dont give any value in the var or const it automatically considers undefined
+
+nice op
+*/
+
+
+
+
+
+
+console.log(accountId);
+console.table([accountId, accountName, accountdetails,hellouser]);
