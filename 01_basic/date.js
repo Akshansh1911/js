0f1e7eab9e57a552c468fae0f2deda1d597b1b68
AKSHANSH GUPTA <91845143+Akshansh1911@users.noreply.github.com>
2024-01-04 14:41:10 +0000

    date

---
+++ 01_basic/date.js
@@ -0,0 +1,12 @@
+let mydate = new Date()
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toISOString());
+
+
+
+let create = new Date(2024, 1, 24)
+console.log(create.toString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
