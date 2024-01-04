9069c66fc6a3a8e7c084e9b3c29bb34c87a6b5eb
AKSHANSH GUPTA <91845143+Akshansh1911@users.noreply.github.com>
2024-01-04 15:26:18 +0000

    arrays

---
+++ 02_basic/01arrays.js
@@ -0,0 +1,37 @@
+/*
+square brackets. []
+parenthesis ()
+curly braces {}
+
+*/
+
+const myArr = [4,5,2,6,8,3,9]
+
+const newArr = new Array(432,6,765,78)
+//array can have mix data types like string numbers boolen
+// arrays are resizable it can be change 
+//zero based indexing 
+// in javascript arrays are not associative arrays  mean u cannot access using string values 
+
+
+//Arrays Methods
+
+myArr.push(6,7)
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
+
+myArr.unshift(8) // helps to add value in first index of array
+console.log(myArr);
+myArr.shift() //remove first index array
+console.log(myArr);
+console.log(myArr.includes(9))//output comes in true or false
+
+
+console.log(myArr.indexOf(2))
+
+
+
+const newArray = myArr.join()// join help to convert arrays into string 
+console.log(typeof(newArray));
