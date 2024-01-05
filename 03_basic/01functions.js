cf2f27d3218bf84570cbf471e9c11de52169b335
AKSHANSH GUPTA <91845143+Akshansh1911@users.noreply.github.com>
2024-01-05 15:43:16 +0000

    save changes

---
+++ 03_basic/01functions.js
@@ -0,0 +1,23 @@
+function saymyname (){
+    console.log("a");
+    console.log("k");
+    console.log("k");
+    console.log("i");
+}
+
+saymyname()
+
+function addnum(num1, num2){
+//    let result = num1 + num2
+//    return result
+return num1 + num2
+
+}
+const result = addnum(4,6)
+console.log("Result: ", result);
+
+function calculateCartPrice(...num1){ //rest operator ... tripal dot
+    return num1
+}
+
+console.log(calculateCartPrice(2,4,6));
